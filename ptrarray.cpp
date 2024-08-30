1faf09b3eda599c51503be7f8387cef3cb7968c4
Mohit-Rawat-7 <mohit.rawat.btech2023@sitpune.edu.in>
2024-08-30 20:37:23 +0530

    Create ptrarray.cpp

---
+++ ptrarray.cpp
@@ -0,0 +1,17 @@
+ //Mohit Singh Rawat
+ //23070123086
+#include<iostream>
+using namespace std;
+int main()
+{
+    int *ptr;
+    int a[5] = { 1,3,5,9,11};
+    ptr = &a[0];
+    int i;
+    for(i=0 ; i<5 ; i++)
+    {
+        cout << "Element "<< i+1 <<" "<<"="<<" "<<*ptr << endl;
+        ptr ++;
+
+    }
+}
