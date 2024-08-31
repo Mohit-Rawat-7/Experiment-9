4b0ef2719752e2ea4bc0e55d2538654098cf0ba8
Mohit-Rawat-7 <mohit.rawat.btech2023@sitpune.edu.in>
2024-08-31 15:08:08 +0530

    Create withoutptr.cpp

---
+++ withoutptr.cpp
@@ -0,0 +1,17 @@
+//Mohit Singh Rawat
+//23070123086
+#include<iostream>
+using namespace std;
+int main()
+{
+    int *ptr;
+    int a[5] = { 1,2,4,8,9};
+    ptr = &a[0];
+    int i;
+    for(i=0 ; i<5 ; i++)
+    {
+        cout << "Element "<< i+1 <<" "<<"="<<" "<<*(a+i) << endl;
+        ptr ++;
+
+    }
+}
